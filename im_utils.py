8590c9d3925a11c8635ecf3cd2813458e0f8ebec
Praveen Gorthy <praveeng@uber.com>
2017-12-09 14:41:43 -0800

    Perceptive transform and unwarp

--- im_utils.py
+++ im_utils.py
@@ -1,8 +1,14 @@
 import cv2
 
 def read_image(path):
-    return cv2.imread(path)
+    """
+    Reads the image from path and converts to rgb space
+    :param path:
+    :return:
+    """
+    img = cv2.imread(path)
+    return cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-def convert_gray(cn2_bgr_image):
-    return cv2.cvtColor(cn2_bgr_image, cv2.COLOR_BGR2GRAY)
+def convert_gray(cn2_rgb_image):
+    return cv2.cvtColor(cn2_rgb_image, cv2.COLOR_RGB2GRAY)
 
